7dde477d3aad5e7514676b22dbe2446da7cc9f62
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-10-24 23:28:23 +0300

    feature: enhance settings to hold recipe weight and type

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 
-import { Button, Slider, FormControlLabel, Checkbox } from '@material-ui/core';
-import { CheckBoxOutlined, CheckBoxOutlineBlankOutlined } from '@material-ui/icons';
+import {
+  Button,
+  Grid,
+  FormControlLabel,
+  Checkbox,
+  FormGroup,
+  Switch,
+  Card,
+  CardContent,
+} from '@material-ui/core';
 
 function App() {
-  const [hunger, setHunger] = React.useState(40);
-  const [complexity, setComplexity] = React.useState(60);
-  const [hamburger, setHamburger] = React.useState(false);
+  const [isHeavy, setIsHeavy] = React.useState(true);
+  const [isDairy, setIsDairy] = React.useState(false);
+  const [isMeat, setIsMeat] = React.useState(true);
 
   const renderTitle = () => (
     <div className="title">
@@ -17,64 +25,58 @@ function App() {
 
   const renderSettings = () => (
     <div className="settings">
-      {renderHunger()}
-      {renderComplexity()}
-      {renderHamburger()}
+      {renderWeightLevel()}
+      {renderyType()}
     </div>
   );
 
   const renderGenerate = () => (
-    <Button onClick={() => {}} variant="contained" color="secondary">
+    <Button
+      onClick={() => alert(`Generate ${isHeavy ? 'Heavy' : 'Light'}${isDairy ? ', Dairy' : ''}${isMeat ? ', Meat' : ''}!`)}
+      variant="contained"
+      color="secondary"
+      className="generate"
+    >
       Generate!
     </Button>
   );
 
-  const renderHunger = () => (
-    <div className="hunger">
-      Hunger
-      <Slider
-        value={hunger}
-        onChange={(event, newValue) => setHunger(newValue)}
-        step={20}
-        color="secondary"
-      />
+  const renderWeightLevel = () => (
+    <div className="weight">
+      <Grid component="label" container alignItems="center" spacing={1}>
+        <Grid item>Light</Grid>
+        <Grid item>
+          <Switch checked={isHeavy} onChange={(event, newValue) => setIsHeavy(newValue)} />
+        </Grid>
+        <Grid item>Heavy</Grid>
+      </Grid>
     </div>
   );
 
-  const renderComplexity = () => (
-    <div className="complexity">
-      Complexity
-      <Slider
-        value={complexity}
-        onChange={(event, newValue) => setComplexity(newValue)}
-        step={20}
-        color="secondary"
-      />
-    </div>
-  );
-
-  const renderHamburger = () => (
-    <div>
-      <FormControlLabel
-        control={
-          <Checkbox
-            value={hamburger}
-            onChange={(event, newValue) => setHamburger(newValue)}
-            icon={<CheckBoxOutlineBlankOutlined />}
-            checkedIcon={<CheckBoxOutlined />}
-          />
-        }
-        label="Hamburger"
-        className="hamburger"
-      />
+  const renderyType = () => (
+    <div className="type">
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox checked={isDairy} onChange={(event, newValue) => setIsDairy(newValue)} />}
+          label="Dairy"
+        />
+        <FormControlLabel
+          control={<Checkbox checked={isMeat} onChange={(event, newValue) => setIsMeat(newValue)} />}
+          label="Meat"
+        />
+      </FormGroup>
     </div>
   );
 
   return (
     <div className="App">
-      {renderTitle()}
-      {renderSettings()}
-      {renderGenerate()}
+      <Card className="card">
+        <CardContent>
+          {renderTitle()}
+          {renderSettings()}
+          {renderGenerate()}
+        </CardContent>
+      </Card>
     </div>
   );
 }
