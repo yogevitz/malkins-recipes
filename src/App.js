1c2a3724097baa746cf5992a9b8695df82fe03d1
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-10-24 21:55:39 +0300

    feature: add initial format for recipe generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import { Button, Slider, FormControlLabel, Checkbox } from '@material-ui/core';
+import { CheckBoxOutlined, CheckBoxOutlineBlankOutlined } from '@material-ui/icons';
+
 function App() {
+  const [hunger, setHunger] = React.useState(40);
+  const [complexity, setComplexity] = React.useState(60);
+  const [hamburger, setHamburger] = React.useState(false);
+
+  const renderTitle = () => (
+    <div className="title">
+      <h2>Malkin's Recipes</h2>
+    </div>
+  );
+
+  const renderSettings = () => (
+    <div className="settings">
+      {renderHunger()}
+      {renderComplexity()}
+      {renderHamburger()}
+    </div>
+  );
+
+  const renderGenerate = () => (
+    <Button onClick={() => {}} variant="contained" color="secondary">
+      Generate!
+    </Button>
+  );
+
+  const renderHunger = () => (
+    <div className="hunger">
+      Hunger
+      <Slider
+        value={hunger}
+        onChange={(event, newValue) => setHunger(newValue)}
+        step={20}
+        color="secondary"
+      />
+    </div>
+  );
+
+  const renderComplexity = () => (
+    <div className="complexity">
+      Complexity
+      <Slider
+        value={complexity}
+        onChange={(event, newValue) => setComplexity(newValue)}
+        step={20}
+        color="secondary"
+      />
+    </div>
+  );
+
+  const renderHamburger = () => (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox
+            value={hamburger}
+            onChange={(event, newValue) => setHamburger(newValue)}
+            icon={<CheckBoxOutlineBlankOutlined />}
+            checkedIcon={<CheckBoxOutlined />}
+          />
+        }
+        label="Hamburger"
+        className="hamburger"
+      />
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderTitle()}
+      {renderSettings()}
+      {renderGenerate()}
     </div>
   );
 }
